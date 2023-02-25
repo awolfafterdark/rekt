46d77e34e30cb1e0c9a5bfd99a0b079130090ad1
awolfafterdark <32263318+awolfafterdark@users.noreply.github.com>
2023-02-25 09:04:52 -0800

    Update User Management.sh

--- User Management.sh
+++ User Management.sh
@@ -1,104 +1,117 @@
 #!/bin/bash
 
-# Set variables
-API_KEY="AIzaSyCm99fk0vDQlgXMgTM9fzlqXiQWHpdOMKA"
+# Set required credentials
+
 CLIENT_ID="568928957521-m57oil8lfnf2qlq6jnq4blp1ljcq4bc8.apps.googleusercontent.com"
-ENDPOINT="https://us-central1-chatbot-e10c8.cloudfunctions.net"
-TOKEN=""
 
-# Function to generate a token using custom token
-generate_token() {
-  # Get custom token from input
-  read -p "Enter custom token: " custom_token
+REVERSED_CLIENT_ID="com.googleusercontent.apps.568928957521-m57oil8lfnf2qlq6jnq4blp1ljcq4bc8"
 
-  # Send API request to generate a token using custom token
-  response=$(curl -s -X POST \
-  "$ENDPOINT/generateToken" \
+API_KEY="AIzaSyCm99fk0vDQlgXMgTM9fzlqXiQWHpdOMKA"
+
+GCM_SENDER_ID="568928957521"
+
+BUNDLE_ID="com.pb.chatgptmac"
+
+PROJECT_ID="aichat-4fb19"
+
+STORAGE_BUCKET="aichat-4fb19.appspot.com"
+
+GOOGLE_APP_ID="1:568928957521:ios:14ccbf85a464137f73f470"
+
+# Generate a new Firebase authentication token
+
+echo "Generating Firebase authentication token..."
+
+FIREBASE_TOKEN=$(curl "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=$API_KEY" \
+
   -H "Content-Type: application/json" \
-  --data '{"customToken":"'"$custom_token"'"}')
 
-  # Extract token from response and store it in global variable
-  TOKEN=$(echo $response | jq -r '.token')
-}
+  --data-binary "{\"token\":\"$(curl -s -X POST \
 
-# Function to create a new user
-create_user() {
-  # Get user details from input
-  read -p "Enter user email: " email
-  read -p "Enter user password: " password
+                                -H 'Content-Type: application/json' \
 
-  # Send API request to create a new user
-  response=$(curl -s -X POST \
-  "$ENDPOINT/createUser" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $TOKEN" \
-  --data '{"email":"'"$email"'","password":"'"$password"'"}')
+                                -H 'Accept: */*' \
 
-  # Display response
-  echo $response
-}
+                                -H 'Accept-Encoding: gzip, deflate' \
 
-# Function to update an existing user
-update_user() {
-  # Get user details from input
-  read -p "Enter user email: " email
-  read -p "Enter new password: " password
+                                -H 'User-Agent: Chat%20Bot/3.2' \
 
-  # Send API request to update user
-  response=$(curl -s -X PUT \
-  "$ENDPOINT/updateUser" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $TOKEN" \
-  --data '{"email":"'"$email"'","password":"'"$password"'"}')
+                                -H 'Content-Length: 51' \
 
-  # Display response
-  echo $response
-}
+                                -H 'Accept-Language: en-US,en;q=0.9' \
 
-# Function to delete an existing user
-delete_user() {
-  # Get user email from input
-  read -p "Enter user email: " email
+                                -d '{\"message\": \"Hello\"}' \
 
-  # Send API request to delete user
-  response=$(curl -s -X DELETE \
-  "$ENDPOINT/deleteUser" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $TOKEN" \
-  --data '{"email":"'"$email"'"}')
+                                https://us-central1-chatbot-e10c8.cloudfunctions.net/chatbot \
 
-  # Display response
-  echo $response
-}
+                                -w '\n%{http_code}')\",\"returnSecureToken\":true}" \
 
-# Main function
-main() {
-  # Generate token using custom token
-  generate_token
+  -s | jq -r '.idToken')
 
-  # Display options for user management
-  echo "Select an option:"
-  echo "1. Create user"
-  echo "2. Update user"
-  echo "3. Delete user"
-  read -p "Option: " option
+# Prompt user to select a task
 
-  # Call appropriate function based on user's choice
-  case $option in
-    1)
-      create_user
-      ;;
-    2)
-      update_user
-      ;;
-    3)
-      delete_user
-      ;;
-    *)
-      echo "Invalid option. Please try again."
-      ;;
-  esac
-}
+echo "Select a user management task to perform:"
 
-# Call main function
-main
+echo "1. Create user"
+
+echo "2. Update user"
+
+echo "3. Delete user"
+
+read -p "Enter a number: " task
+
+case $task in
+
+  1)
+
+    # Create user
+
+    read -p "Enter email address: " email
+
+    read -p "Enter password: " password
+
+    read -p "Enter display name: " displayName
+
+    echo "Creating user..."
+
+    curl "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$API_KEY" \
+
+      -H "Content-Type: application/json" \
+
+      -d "{\"email\":\"$email\",\"password\":\"$password\",\"displayName\":\"$displayName\",\"returnSecureToken\":true,\"localId\":\"$PROJECT_ID\"}" \
+
+      -H "Authorization: Bearer $FIREBASE_TOKEN"
+
+    ;;
+
+  2)
+
+    # Update user
+
+    read -p "Enter user ID: " userId
+
+    read -p "Enter new display name: " displayName
+
+    echo "Updating user..."
+
+    curl "https://identitytoolkit.googleapis.com/v1/accounts:update?key=$API_KEY" \
+
+      -H "Content-Type: application/json" \
+
+      -d "{\"localId\":\"$userId\",\"displayName\":\"$displayName\",\"idToken\":\"$FIREBASE_TOKEN\"}"
+
+    ;;
+
+  3)
+
+    # Delete user
+
+    read -p "Enter user ID: " userId
+
+    echo "Deleting user..."
+
+    curl "https://identitytoolkit.googleapis.com/v1/accounts:delete?key=$API_KEY" \
+
+      -H "Content-Type: application/json" \
+
+      -d "{\"localId\":\"$
